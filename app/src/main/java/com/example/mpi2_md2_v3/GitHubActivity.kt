73ca7a9f50b3b431c4c48c3023327bf7a7c21f71
Andris Tolmanis <tolmanis@outlook.lv>
2019-04-29 00:21:35 +0300

    Map and repo list functional

---
+++ app/src/main/java/com/example/mpi2_md2_v3/GitHubActivity.kt
@@ -0,0 +1,93 @@
+package com.example.mpi2_md2_v3
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_git_hub.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import java.util.*
+
+
+class GitHubActivity : AppCompatActivity() {
+    var list: MutableList<Repository> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_hub)
+        setSupportActionBar(toolbar)
+
+        val adapter = ArrayAdapter(this, R.layout.list_item, list)
+
+        val service = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(GitHubService::class.java)
+
+        service.retrieveRepositories("andristolmanis")
+            .enqueue(object : Callback<List<Repository>> {
+                override fun onResponse(call: Call<List<Repository>>, response: Response<List<Repository>>) {
+                    response.body()?.forEach {
+                        println(it)
+                        list.add(it)
+                    }
+                    listView.setAdapter(adapter)
+                }
+                override fun onFailure(call: Call<List<Repository>>, t: Throwable) = t.printStackTrace()
+            })
+
+
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_map -> {
+                val intent = Intent(this, MapsActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+
+                true
+            }
+            R.id.action_github -> {
+                Toast.makeText(this, "Already here.", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    interface GitHubService {
+        @GET("/users/{user}/repos")
+        fun retrieveRepositories(@Path("user") user: String): Call<List<Repository>>
+    }
+
+}
+
+data class Repository(val name: String, val owner: Ow)
+
+data class Ow(val login: String)
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/mpi2_md2_v3/MapsActivity.kt
@@ -0,0 +1,103 @@
+package com.example.mpi2_md2_v3
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import kotlinx.android.synthetic.main.activity_git_hub.*
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_map -> {
+                Toast.makeText(this, "Already here.", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.action_github -> {
+                val intent = Intent(this, GitHubActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        map.getUiSettings().setZoomControlsEnabled(true)
+        map.setOnMarkerClickListener(this)
+
+        setUpMap()
+    }
+
+    override fun onMarkerClick(p0: Marker?) = false
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        map.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 13f))
+            }
+        }
+
+    }
+
+
+
+
+
+
+}
+
